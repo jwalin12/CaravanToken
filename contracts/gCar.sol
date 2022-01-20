66acdf235406c4f2a6d0d542ece9a4f5171c2f9f
jwalin12 <jwalin12@berkeley.edu>
2022-01-19 16:59:26 -0800

    feat: inital gCAR ERC20 contract

---
+++ contracts/gCar.sol
@@ -0,0 +1,30 @@
+pragma solidity >= 0.8.0;
+
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+
+
+
+contract gCAR is ERC20Capped {
+
+
+    address private minter;
+
+
+    constructor(address _minter, address vestingContract, address airdropManager, address treasury) public ERC20("Caravan governance", "gCAR") ERC20Capped(500000) {
+        minter = _minter;
+
+        //TODO: mint initial supply to vesting contracts + airdrop holder + treasury based on distribution
+    }
+
+    function mint(address to, uint256 amount) external {
+        require(msg.sender == minter);
+
+        _mint(to, amount);
+
+    }
+
+
+}
